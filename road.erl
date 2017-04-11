83e67db851e41fd9b500153194fa0b1c7faea893
Leonel Mendez <s_mendezl@globalhitss.com>
2017-04-11 08:44:31 -0500

    UPDATE: Finished shortest path algorithm

--- road.erl
+++ road.erl
@@ -1,10 +1,11 @@
 -module(road).
--export([main/0,parse_map/1,group_vals/2]).
-
-main() ->
-    File = "road.txt",
-    {ok, Binary} = file:read_file(File),
-    parse_map(Binary).
+-export([main/1]).
+ 
+main([FileName]) ->
+    {ok, Bin} = file:read_file(FileName),
+    Map = parse_map(Bin),
+    io:format("~p~n",[optimal_path(Map)]),
+    erlang:halt(0).
 
 parse_map(Binary) when is_binary(Binary) ->
     parse_map(binary_to_list(Binary));
@@ -18,3 +19,17 @@ group_vals([],Acc) ->
 
 group_vals([A,B,C | Rest], Acc) ->
     group_vals(Rest, [{A,B,C} | Acc]).
+
+shortest_step({A,B,X}, {{DistA,PathA}, {DistB,PathB}}) ->
+    OptA1 = {DistA + A, [{a,A}|PathA]},
+    OptA2 = {DistB + B + X, [{x,X}, {b,B}|PathB]},
+    OptB1 = {DistB + B, [{b,B}|PathB]},
+    OptB2 = {DistA + A + X, [{x,X}, {a,A}|PathA]},
+    {erlang:min(OptA1, OptA2), erlang:min(OptB1, OptB2)}.
+
+optimal_path(Map) ->
+    {A,B} = lists:foldl(fun shortest_step/2, {{0,[]}, {0,[]}}, Map),
+    {_Dist,Path} = if hd(element(2,A)) =/= {x,0} -> A;
+    hd(element(2,B)) =/= {x,0} -> B
+    end,
+    lists:reverse(Path).
